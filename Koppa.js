c02190504a84e2e6865a205c2bcd114576edc0d6
Quentin Adeline <quentin.adeline18@gmail.com>
2023-04-19 23:13:05 +0200

    tel

--- Koppa.js
+++ Koppa.js
@@ -68,15 +68,12 @@ switchElement.addEventListener('click', function () {
     rootElement.style.setProperty('--couleur-text', '#BCBDBF');
     rootElement.style.setProperty('--couleur-fonce', '#EDEEF0');
     rootElement.style.setProperty('--couleur-gros-texte', '#FFF');
-    
   }
   else {      /* Claire */
     rootElement.style.setProperty('--couleur-fond', '#EDEEF0');
     rootElement.style.setProperty('--couleur-text', '#555555');
     rootElement.style.setProperty('--couleur-fonce', '#000');
     rootElement.style.setProperty('--couleur-gros-texte', '#FFF');
-
-
   }
 });
 
