23ef5fdce343721f48fcb64e1d43e0f24fed0962
Quentin Adeline <quentin.adeline18@gmail.com>
2023-10-11 23:09:40 +0800

    Site V3.0

---
+++ Style/js/script.js
@@ -0,0 +1,103 @@
+function DisplayBlog(){
+  document.getElementById("Titre_Projet").innerHTML = "Travel blog ~ Malaysia 2023";
+  document.getElementById("Texte_Projet").innerHTML = "Passionate about photography, I had the chance to discover South Asia in 2023 during my student mobility. <br>An opportunity for me to capture some magnificent moments with my Connon EOS 2000D camera.<br>It's up to you to discover these magnificent landscapes.";
+  document.getElementById("Image_Projet").src = "";
+  document.getElementById("Lien_Projet").href = "blog.html";
+
+  document.getElementById("Bouton_Cyber").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Programmes").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Conte").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Certifications").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Blog").style.backgroundImage = "url('images/Bouton_Orange.png')";
+  document.getElementById("Bouton_Frigs").style.backgroundImage = "url('images/Bouton_Vert.png')";
+}
+
+function DisplayCyber(){
+    document.getElementById("Titre_Projet").innerHTML = "Cyber ​​Report 2022";
+    document.getElementById("Texte_Projet").innerHTML = "As a cybersecurity intern, I observed the digital security challenges that businesses and individuals face. In this report, I discuss the different types of attacks, their consequences and preventative measures to protect yourself. My goal is to educate users about online security risks and help them protect themselves.";
+    document.getElementById("Image_Projet").src = "images/projects/Project_Cyber.png";
+    document.getElementById("Lien_Projet").href = "Documents/Aperçu des cyber menaces les plus répandues sur l’année 2022.pdf";
+
+    document.getElementById("Bouton_Cyber").style.backgroundImage = "url('images/Bouton_Orange.png')";
+    document.getElementById("Bouton_Programmes").style.backgroundImage = "url('images/Bouton_Vert.png')";
+    document.getElementById("Bouton_Conte").style.backgroundImage = "url('images/Bouton_Vert.png')";
+    document.getElementById("Bouton_Certifications").style.backgroundImage = "url('images/Bouton_Vert.png')";
+    document.getElementById("Bouton_Blog").style.backgroundImage = "url('images/Bouton_Vert.png')";
+    document.getElementById("Bouton_Frigs").style.backgroundImage = "url('images/Bouton_Vert.png')";
+
+}
+
+function DisplayProgramms(){
+  document.getElementById("Titre_Projet").innerHTML = "My GitHub";
+  document.getElementById("Texte_Projet").innerHTML = "It presents a variety of projects developed during my training in engineering school. It includes my first year transversal project, a Python game, as well as a Takuzu game. You will also find other projects there, which show my ability to work on group projects and to implement varied technical skills...";
+  document.getElementById("Image_Projet").src = "images/projects/Project_Programmation.png";
+  document.getElementById("Lien_Projet").href = "https://github.com/Quentinadl";
+
+  document.getElementById("Bouton_Cyber").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Programmes").style.backgroundImage = "url('images/Bouton_Orange.png')";
+  document.getElementById("Bouton_Conte").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Certifications").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Blog").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Frigs").style.backgroundImage = "url('images/Bouton_Vert.png')";
+}
+
+function DisplayConte(){
+  document.getElementById("Titre_Projet").innerHTML = "Le chat et la souris";
+  document.getElementById("Texte_Projet").innerHTML = "Designed for an audience of 6 to 8 years old, my tale entitled The Cat and the Mouse is also aimed at an older audience understanding the underside of a real social problem. I won't tell you any more, and let you discover for yourself the wonderful story of the mouse Sète and the cat Malo...";
+  document.getElementById("Image_Projet").src = "images/projects/Project_Comte.png";
+  document.getElementById("Lien_Projet").href = "Documents/Le_chat_et_la_souris.pdf";
+
+  document.getElementById("Bouton_Cyber").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Programmes").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Conte").style.backgroundImage = "url('images/Bouton_Orange.png')";
+  document.getElementById("Bouton_Certifications").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Blog").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Frigs").style.backgroundImage = "url('images/Bouton_Vert.png')";
+}
+
+function DisplayCertification(){
+  document.getElementById("Titre_Projet").innerHTML = "ANSSI MOOC";
+  document.getElementById("Texte_Projet").innerHTML = "This training allowed me to acquire the first knowledge in cybersecurity, covering topics such as risk management, cryptography and systems security.";
+  document.getElementById("Image_Projet").src = "images/projects/Project_Certification.png";
+  document.getElementById("Lien_Projet").href = "https://www.ssi.gouv.fr/actualite/la-plateforme-du-mooc-secnumacademie-de-lanssi-va-evoluer/";
+
+  document.getElementById("Bouton_Cyber").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Programmes").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Conte").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Certifications").style.backgroundImage = "url('images/Bouton_Orange.png')";
+  document.getElementById("Bouton_Blog").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Frigs").style.backgroundImage = "url('images/Bouton_Vert.png')";
+}
+
+function DisplayFrigs(){
+  document.getElementById("Titre_Projet").innerHTML = "Mobile app";
+  document.getElementById("Texte_Projet").innerHTML = "We are a team of engineering students who created Frig's, an innovative app aimed at solving the problem of food waste. Frig's allows everyone to empty their fridge while reducing the amount of food waste in households. Our prototype, produced as a team as part of a semester project.";
+  document.getElementById("Image_Projet").src = "images/projects/Project_Frig's.png";
+  document.getElementById("Lien_Projet").href = "Documents/Frigs.pdf";
+
+  document.getElementById("Bouton_Cyber").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Programmes").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Conte").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Certifications").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Blog").style.backgroundImage = "url('images/Bouton_Vert.png')";
+  document.getElementById("Bouton_Frigs").style.backgroundImage = "url('images/Bouton_Orange.png')";
+}
+
+
+function DisplayContact(){
+  if (document.getElementById("Contact").style.display == "none"){
+    document.getElementById("Contact").style.display = "flex";
+    document.getElementById("contact").style.color = "#CD8E6B";
+  }
+  else{
+    document.getElementById("Contact").style.display = "none";
+    document.getElementById("contact").style.color = "#333";
+
+  }
+}
+
+function HideContact(){
+  document.getElementById("Contact").style.display = "none";
+  document.getElementById("contact").style.color = "#333";
+}
+
